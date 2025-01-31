5a3b1fbbc0d511ec22dbcaca0bad91c305d83127
guevaraeduard97 <guevaraeduard97@gmail.com>
2025-01-30 21:24:44 -0400

    empieza el usuario

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+//AQUI USAR EL SOCKET
 </script>
 
 <template>
-
   <RouterView />
-
-
 </template>
---
+++ src/views/UserView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// ... existing code ...
+</script>
+
+<template>
+  <div
+    style="
+      background-image: url('../../public/img/fondo.jpg');
+      background-size: cover;
+      background-position: center;
+      height: 100vh;
+    "
+  >
+    <!-- Contenido ad icional aquí -->
+    <div class="flex items-center justify-center h-full">
+      <div class="bg-white rounded-lg shadow-lg p-6 max-w-sm text-center">
+        <img
+          src="../../public/img/avatar.png"
+          alt="Imagen de perfil"
+          class="w-24 h-24 rounded-full mx-auto mb-4"
+        />
+        <h2 class="text-xl font-semibold">Bienvenido</h2>
+        <p class="text-gray-600">LLene el formulario para continuar</p>
+      </div>
+    </div>
+  </div>
+</template>
